4ef9426ef21902518fed3f756d273232521dd6f3
marijal74 <lakic.marija98@gmail.com>
2025-01-08 20:41:35 +0100

    Init project and add gh action for building and tagging an image.

---
+++ workflow-scripts/build-and-tag-image.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+# Build a docker container and push it to ECR
+docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7} -f ./Dockerfile .
+docker push $ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7}
+echo "image=$ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7}" >> $GITHUB_OUTPUT
