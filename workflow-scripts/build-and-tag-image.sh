4b32e17e1af95a8ca93393dd4e890b9c705f39d9
marijal74 <lakic.marija98@gmail.com>
2025-01-10 21:30:17 +0100

    Fix adding permission

--- workflow-scripts/build-and-tag-image.sh
+++ workflow-scripts/build-and-tag-image.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/bash
 # Build a docker container and push it to ECR
-echo pwd
-chmod +x workflow-scripts/build-and-tag-image.sh
 docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7} -f ./Dockerfile .
 docker push $ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7}
 echo "image=$ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7}" >> $GITHUB_OUTPUT
