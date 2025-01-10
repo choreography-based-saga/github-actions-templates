546c0a27a9b88c91e94f2e135959b1daa3f271ef
marijal74 <lakic.marija98@gmail.com>
2025-01-10 21:22:07 +0100

    Log pwd

--- workflow-scripts/build-and-tag-image.sh
+++ workflow-scripts/build-and-tag-image.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 # Build a docker container and push it to ECR
-pwd
+echo pwd
 chmod +x workflow-scripts/build-and-tag-image.sh
 docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7} -f ./Dockerfile .
 docker push $ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7}
