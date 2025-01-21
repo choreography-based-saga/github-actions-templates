126cf32da08c4974a6683be95a3906a9dda6540f
marijal74 <lakic.marija98@gmail.com>
2025-01-21 21:35:18 +0100

    Change image naming

--- workflow-scripts/build-and-tag-image.sh
+++ workflow-scripts/build-and-tag-image.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
 # Build a docker container and push it to ECR
-docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7} -f ./Dockerfile .
-docker push $ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7}
-echo "image=$ECR_REGISTRY/$ECR_REPOSITORY:${IMAGE_TAG::7}" >> $GITHUB_OUTPUT
+docker build -t $ECR_REGISTRY/$ECR_REPOSITORY:${REPO_NAME}-${IMAGE_TAG::7} -f ./Dockerfile .
+docker push $ECR_REGISTRY/$ECR_REPOSITORY:${REPO_NAME}-${IMAGE_TAG::7}
+echo "image=$ECR_REGISTRY/$ECR_REPOSITORY:${REPO_NAME}-${IMAGE_TAG::7}" >> $GITHUB_OUTPUT
